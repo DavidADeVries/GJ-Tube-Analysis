ad646a7d7e3ff5525e45d6abc7da1c35b1f4fd25
DavidADeVries <ddevries@uwaterloo.ca>
2015-07-23 15:16:10 -0400

    Changed constants, naming, GUI changes for GIANT
    - constants now have two levels to make them easier to change (MATLAB
    prevents these being in one file.
    - naming switched to GAS-SAM for files and figures
    - GIANT has added study switching buttons, snapshot button, so GUI
    updated accordingly

--- GUI/Constants.m
+++ GUI/Constants.m
@@ -1,19 +1,21 @@
 classdef Constants
     %Constants stores the constants
     
+        
     properties (Constant = true)
         % for dev
-        GIANT_PATH = '/data/projects/General Image Analysis Toolkit/GIANT/metadata/';
+        GIANT_PATH = strcat(HighLevelConstants.BASE_DIRECTORY, '/General Image Analysis Toolkit/GIANT/metadata/');
+        ALLOW_CLOSE = true;
         
         % for released
-        %GIANT_PATH = '/data/projects/General Image Analysis Toolkit/Current Release/metadata/';
+        %GIANT_PATH = strcat(HighLevelConstants.BASE_DIRECTORY, /General Image Analysis Toolkit/Current Release/metadata/');
+        %ALLOW_CLOSE = false;
         
         
-        
-        
-        SAVED_PATIENTS_DIRECTORY = '/data/projects/GJ Tube/GJ Tube Study/Saved Patient Analysis/'; %make sure it's absolute and ends with '/'
-        RAW_DATA_DIRECTORY = '/data/projects/GJ Tube/GJ Tube Study/Raw Data/';
-        CSV_EXPORT_DIRECTORY = '/data/projects/GJ Tube/GJ Tube Study/Exported Spreadsheets/';
+        SAVED_PATIENTS_DIRECTORY = strcat(HighLevelConstants.BASE_DIRECTORY, HighLevelConstants.STUDY_DIRECTORY, '/Saved Patient Analysis/'); %make sure it's absolute and ends with '/'
+        RAW_DATA_DIRECTORY = strcat(HighLevelConstants.BASE_DIRECTORY, HighLevelConstants.STUDY_DIRECTORY, '/Raw Data/');
+        CSV_EXPORT_DIRECTORY = strcat(HighLevelConstants.BASE_DIRECTORY, HighLevelConstants.STUDY_DIRECTORY, '/Exported Spreadsheets/');
+        SNAPSHOT_DIRECTORY = strcat(HighLevelConstants.BASE_DIRECTORY, HighLevelConstants.STUDY_DIRECTORY, '/Snapshots/');
         
         SAVE_TITLE_SUGGESTION = 'Tube Analysis';
         
